e5c8b91d0c116e798f55756e859bbdba8d2264bc
Waris Oloyede <oloyedewaris@gmail.com>
2022-06-28 09:54:52 +0100

    adjust svg footer

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,10 +6,10 @@ function Home() {
   return (
     <div className='home_main'>
       <div className='logo_upright'>
-        <Logo width={'60vw'} height={'100px'} />
+        <Logo width={'80vw'} height={'150px'} />
       </div>
       <div className='logo_inverted'>
-        <Logo width={'60vw'} height={'100px'} />
+        <Logo width={'80vw'} height={'500px'} />
       </div>
     </div>
   )
